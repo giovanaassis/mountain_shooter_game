6bdae3dbf0581019c3b284ebbfe82303f4c8ab3f
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-19 23:56:23 -0300

    feat: added the menu background

--- code/Game.py
+++ code/Game.py
@@ -1,17 +1,20 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
+
+
             # CHECK FOR ALL EVENTS
             # for event in pygame.event.get():
             #    if event.type == pygame.QUIT:
--- code/Menu.py
+++ code/Menu.py
@@ -1,9 +1,13 @@
+import pygame
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-
+        self.surf = pygame.image.load("./assets/MenuBg.png")
+        self.rect = self.surf.get_rect()
 
     def run(self):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
         pass

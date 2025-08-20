eaa65e9e1ca38fb5b9f9fef1ab0679a399343af7
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-19 23:32:21 -0300

    docs: added Game and menu files

---
+++ code/Game.py
@@ -0,0 +1,19 @@
+import pygame
+
+from code.Menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            # CHECK FOR ALL EVENTS
+            # for event in pygame.event.get():
+            #    if event.type == pygame.QUIT:
+            #         pygame.quit()  # CLOSE THE WINDOW
+            #         quit()
--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import pygame
+from code.Game import Game
 
-print("SETUP START")
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print("SETUP END")
-
-print("LOOP START")
-while True:
-    # CHECK FOR ALL EVENTS
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # CLOSE THE WINDOW
-            quit()
+game = Game()

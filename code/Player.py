901851e544968e0bdd1fdd33c3598ba7db17061a
giovanaassis <giovana_2011_11@hotmail.com>
2025-08-22 21:44:21 -0300

    feat: created the PlayerShot and EnemyShot

--- code/Player.py
+++ code/Player.py
@@ -1,18 +1,18 @@
 import pygame
 
 from code.Const import WIN_HEIGHT, WIN_WIDTH, ENTITY_SPEED, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT, \
-    PLAYER_KEY_UP
+    PLAYER_KEY_UP, ENTITY_SHOT_DELAY, PLAYER_KEY_SHOOT
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
-
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def update(self):
         pass
-
 
     def move(self):
         pressed_key = pygame.key.get_pressed()
@@ -25,3 +25,13 @@ class Player(Entity):
         if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:
             self.rect.centerx += ENTITY_SPEED[self.name]
         pass
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+            return None
+        return None
